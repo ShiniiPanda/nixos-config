6e214412843259ce22b766aa7dcf1ed15a310530
Shinii <celisiagaming@gmail.com>
2024-08-03 15:15:31 +0300

    Updated flakes to take in special args

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    stylix.url = "github:danth/stylix";
 
      home-manager = {
        url = "github:nix-community/home-manager";
@@ -11,15 +12,23 @@
      };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, stylix, ... }@inputs: 
+  let
+    userSettings = {
+      profile = "panda";
+    };
+  in
+   {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {
         inherit inputs;
+        inherit userSettings;
       };
       modules = [
         ./profiles/default/configuration.nix
         nixos-hardware.nixosModules.asus-rog-strix-g713ie
-        inputs.home-manager.nixosModules.default
+        home-manager.nixosModules.default
+        stylix.nixosModules.stylix
       ];
     };
   };
