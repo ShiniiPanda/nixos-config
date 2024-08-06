a07babecd735f3eef283c453240fb504743b66a3
Shinii <celisiagaming@gmail.com>
2024-08-06 17:08:00 +0300

    Fixed typo with kitty module

--- modules/user/terminal/kitty.nix
+++ modules/user/terminal/kitty.nix
@@ -5,7 +5,7 @@
   home.packages = with pkgs; [ kitty ];
   programs.kitty.enable = true;
   programs.kitty.settings = {
-    background_opacity = lib.mkFOrce "0.85";
+    background_opacity = lib.mkForce "0.85";
   };
 
 }
