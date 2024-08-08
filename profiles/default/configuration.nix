73b94a0d7cee4558edd0141ebe2645ea8223ad3f
Shinii <celisiagaming@gmail.com>
2024-08-08 21:44:45 +0300

    Configured my primary profile to use shell variable defined in flake

--- profiles/default/configuration.nix
+++ profiles/default/configuration.nix
@@ -115,6 +115,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.panda = {
+     shell = pkgs.${userSettings.shell};
      isNormalUser = true;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
