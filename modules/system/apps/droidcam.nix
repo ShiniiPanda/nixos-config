3e8958fa46a22669684937d895849245a242dc4e
Shinii <celisiagaming@gmail.com>
2024-08-20 22:07:39 +0300

    Removed some trailing space! grrr!!!

--- modules/system/apps/droidcam.nix
+++ modules/system/apps/droidcam.nix
@@ -8,7 +8,7 @@
   environment.systemPackages = with pkgs; [ v4l-utils ];
   security.polkit.enable = true;
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=2 video_nr=1,2 card_label="Droidcam,OBS Virtual Cam" exclusive_caps=1,1 
+    options v4l2loopback devices=2 video_nr=1,2 card_label="Droidcam,OBS Virtual Cam" exclusive_caps=1,1
   '';
 
 }
