d84705296faa26ecbd0ff61208cd95b9178c79a4
Shinii <celisiagaming@gmail.com>
2024-08-20 22:42:06 +0300

    Removed trailing space

--- profiles/default/configuration.nix
+++ profiles/default/configuration.nix
@@ -21,7 +21,7 @@
 
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+   boot.loader.efi.canTouchEfiVariables = true;
   
 
    boot.loader.grub = {
