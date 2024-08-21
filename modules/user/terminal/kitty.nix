ed37558361de618dd733ff24680516874b9b8824
Shinii <celisiagaming@gmail.com>
2024-08-21 18:44:06 +0300

    zero padding on kitty is hersey ong fr!

--- modules/user/terminal/kitty.nix
+++ modules/user/terminal/kitty.nix
@@ -7,6 +7,7 @@
     settings = {
       background_opacity = lib.mkForce "0.85";
       confirm_os_window_close = 0;
+      window_padding_width = "10";
     };
     shellIntegration.enableZshIntegration = true;
   };
