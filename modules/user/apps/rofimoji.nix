245fd75699427ff9d5a51ed4a48a47e229b39f3b
Shinii <celisiagaming@gmail.com>
2024-08-22 21:17:58 +0300

    Added rofi emoji menu!!!!

--- modules/user/apps/rofimoji.nix
+++ modules/user/apps/rofimoji.nix
@@ -2,5 +2,10 @@
 {
 
   home.packages = with pkgs; [ rofimoji ];
+  # Configuring it to copy straight into wl-clipboard god bless
+  home.file.".config/rofimoji.rc".text = ''
+    action = copy
+    skin-tone = moderate
+  '';
 
 }
