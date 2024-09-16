30881d8c8fd0e789ac3b52b7ab9c785c59fbbac1
Shinii <celisiagaming@gmail.com>
2024-09-16 21:38:04 +0300

    Adjusted stylix module to allow for some funny stuff

--- modules/system/style/stylix.nix
+++ modules/system/style/stylix.nix
@@ -1,17 +1,21 @@
 { userSettings, pkgs, lib, ...}:
 let
   inherit (pkgs) fetchurl;
-  theme = ../../../themes/${userSettings.theme}/theme.yaml;
+  regex = ''^${userSettings.theme}\.[^\s]+$'';
+  file = builtins.toString (builtins.filter (f: builtins.match regex f != null) (builtins.attrNames (builtins.readDir ../../../themes/${userSettings.theme})));
+  fileExists = if file == "" then false else true;
+  # theme = ../../../themes/${userSettings.theme}/theme.yaml;
   polarity = lib.removeSuffix "\n" (builtins.readFile ../../../themes/${userSettings.theme}/polarity.txt);
-  background = builtins.readFile ../../../themes/${userSettings.theme}/background.txt;
-  background256 = builtins.readFile ../../../themes/${userSettings.theme}/backgroundsha256.txt;
+  background = if !fileExists then builtins.readFile ../../../themes/${userSettings.theme}/background.txt else "";
+  background256 = if !fileExists then builtins.readFile ../../../themes/${userSettings.theme}/backgroundsha256.txt else "";
 in
 {
   # Credit goes to librephoenix aka Emmet for helping me tons with organizing my styles!
   stylix.enable = true;
-  stylix.image = fetchurl {
-    url = background;
-    sha256 = background256; 
+  stylix.image = if fileExists then ../../../themes/${userSettings.theme}/${file}
+    else fetchurl {
+      url = background;
+      sha256 = background256; 
   }; 
 #  stylix.base16Scheme = theme;
   stylix.polarity = polarity;
