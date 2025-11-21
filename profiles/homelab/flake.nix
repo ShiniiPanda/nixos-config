c126d8fc94ac04e71031ab4e10df78388b34f595
Shinii <celisiagaming@gmail.com>
2025-11-21 16:22:10 +0200

    Added homelab profile

---
+++ profiles/homelab/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Nixos homelab config flake";
+
+  inputs = {
+    nixpkgs.url = "github:NIXOS/nixpkgs/nixos-25.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    stylix.url = "github:danth/stylix";
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, stylix, ... }@inputs:
+    let
+      userSettings = {
+        profile = "panda";
+        theme = "moonman";
+        shell = "zsh";
+        terminal = "kitty";
+
+        wm = "hyprland";
+        launcher = "rofi";
+        browser = "brave";
+        fileManager = "thunar";
+        notification = "mako";
+      };
+    in {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+          inherit userSettings;
+        };
+        modules = [
+          ./hosts/configuration.nix
+          # nixos-hardware.nixosModules.asus-rog-strix-g713ie
+          home-manager.nixosModules.default
+        ];
+      };
+    };
+}
