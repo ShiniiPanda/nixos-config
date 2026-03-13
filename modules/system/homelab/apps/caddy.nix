e38fb1ca102fb6114f7a4523f99fac469001589d
shinii-homelab <celisiagaming@gmail.com>
2026-03-13 18:38:24 +0200

    typo in redacted

--- modules/system/homelab/apps/caddy.nix
+++ modules/system/homelab/apps/caddy.nix
@@ -1,12 +1,11 @@
-{ lib, pkgs, ... }:
-{
+{ lib, pkgs, ... }: {
 
   services.caddy = {
     enable = true;
-    virtualHosts."boss.shinii.space".extraConfig = ``
+    virtualHosts."boss.shinii.space".extraConfig = ''
       root * /srv/www/static
       file_server
-    ``;
+    '';
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
