69bd38fa10ccf230d477cebd8c38943d2daca05d
Shinii <celisiagaming@gmail.com>
2026-06-18 19:28:35 +0300

    Reworking themes a little, let me cook...

--- modules/system/style/stylix.nix
+++ modules/system/style/stylix.nix
@@ -1,23 +1,46 @@
-{ userSettings, pkgs, lib, ...}:
+{ userSettings, pkgs, lib, ... }:
 let
   inherit (pkgs) fetchurl;
-  regex = ''^${userSettings.theme}\.[^\s]+$'';
-  file = builtins.toString (builtins.filter (f: builtins.match regex f != null)
-    (builtins.attrNames (builtins.readDir ../../../themes/${userSettings.theme})));
-  fileExists = if file == "" then false else true;
-  # theme = ../../../themes/${userSettings.theme}/theme.yaml;
-  polarity = lib.removeSuffix "\n" (builtins.readFile ../../../themes/${userSettings.theme}/polarity.txt);
-  background = if !fileExists then builtins.readFile ../../../themes/${userSettings.theme}/background.txt else "";
-  background256 = if !fileExists then builtins.readFile ../../../themes/${userSettings.theme}/backgroundsha256.txt else "";
-in
-{
+
+  imageRegex = "^${userSettings.theme}\\.[^[:space:]]+$";
+  image = builtins.toString
+    (builtins.filter (f: builtins.match imageRegex f != null)
+      (builtins.attrNames
+        (builtins.readDir ../../../themes/${userSettings.theme})));
+  imageExists = if file == "" then false else true;
+
+  themeYAML = ../../../themes/${userSettings.theme}/theme.yaml;
+  themeJSON = ../../../themes/${userSettings.theme}/theme.json;
+  theme = if builtins.pathExists themeYAML then
+    themeYAML
+  else if builtins.pathExists themeJSON then
+    themeJSON
+  else
+    null;
+
+  polarity = lib.removeSuffix "\n"
+    (builtins.readFile ../../../themes/${userSettings.theme}/polarity.txt);
+
+  background = if !fileExists then
+    builtins.readFile ../../../themes/${userSettings.theme}/background.txt
+  else
+    "";
+
+  background256 = if !fileExists then
+    builtins.readFile ../../../themes/${userSettings.theme}/backgroundsha256.txt
+  else
+    "";
+
+in {
   # Credit goes to librephoenix aka Emmet for helping me with organizing my styles!
   stylix.enable = true;
-  stylix.image = if fileExists then ../../../themes/${userSettings.theme}/${file}
-    else fetchurl {
+  stylix.image = if fileExists then
+    ../../../themes/${userSettings.theme}/${file}
+  else
+    fetchurl {
       url = background;
-      sha256 = background256; 
-  }; 
-#  stylix.base16Scheme = theme;
+      sha256 = background256;
+    };
+  stylix.base16Scheme = lib.mkIf (theme != null) theme;
   stylix.polarity = polarity;
 }
