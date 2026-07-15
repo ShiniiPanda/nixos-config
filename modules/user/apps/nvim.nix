2e6479e4fe8aca794438ab1bfa93e78c382638a4
Shinii <celisiagaming@gmail.com>
2026-07-15 18:54:20 +0300

    neovim no longer home manager'ed

--- modules/user/apps/nvim.nix
+++ modules/user/apps/nvim.nix
@@ -1,22 +1,29 @@
-{ config, lib, pkgs, ... }:
-let 
-	configDirectory = pkgs.fetchFromGitHub {
-		owner = "ShiniiPanda";
-		repo = "AstroNvim-template";
-		rev = "main";
-		sha256 = "sha256-oWTi3ht8HTtCB0LYWQ8Tu83sKAa0LWxoTLx7H/1E+ds";
-	};
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  configDirectory = pkgs.fetchFromGitHub {
+    owner = "ShiniiPanda";
+    repo = "AstroNvim-template";
+    rev = "main";
+    sha256 = "sha256-oWTi3ht8HTtCB0LYWQ8Tu83sKAa0LWxoTLx7H/1E+ds";
+  };
 in
 {
-	programs.neovim = {
-		enable = true;
-		plugins = with pkgs.vimPlugins; [
-			astroui
-			astrolsp
-			astrocore
-			astrotheme
-		];
-	};
+  programs.neovim = {
+    enable = true;
+    # plugins = with pkgs.vimPlugins; [
+    #   astroui
+    #   astrolsp
+    #   astrocore
+    #   astrotheme
+    # ];
+    withRuby = true;
+    withPython3 = true;
+  };
 
-	home.file.".config/nvim".source = configDirectory;
+  # home.file.".config/nvim".source = configDirectory;
 }
